69987f3e94dac96ea830a68009862202cbbe541f
Antonin NYCZ <antonin.nycz-prestataire@bforbank.com>
2015-12-17 16:33:51 +0100

    Exemple 2 JSON SQL : sql request

--- src/main/java/com/meow/Main.java
+++ src/main/java/com/meow/Main.java
@@ -1,17 +1,29 @@
 package com.meow;
 
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.SQLContext;
+
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
         JavaSparkContext sc = new JavaSparkContext("local[*]", "applicationName");
         SQLContext sqlContext = new SQLContext(sc);
-        DataFrame df = sqlContext.read().json("src/main/resources/data/arbres.json");
-        df.show();
-        df.printSchema();
-        df.select("fields.nom_commun", "fields.famille").show();
+        JavaRDD<Person> personRdd = sc.textFile("src/main/resources/data/persons.txt")
+                .map(line -> {
+                    String[] colums = line.split(",");
+                    return new Person(colums[0], Integer.parseInt(colums[1]));
+                });
+        DataFrame df = sqlContext.createDataFrame(personRdd, Person.class);
+        df.registerTempTable("people");
+
+        DataFrame teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19");
+        List<String> teens = teenagers.javaRDD()
+                .map(row -> "Name : " + row.getString(0))
+                .collect();
+        teens.stream().forEach(System.out::println);
     }
 }
