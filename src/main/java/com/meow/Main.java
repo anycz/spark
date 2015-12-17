66cf140ea6e4f01272689cbf794652bd46010490
Antonin NYCZ <antonin.nycz-prestataire@bforbank.com>
2015-12-17 16:47:37 +0100

    Exemple 3 SQL : schema dynamique

--- src/main/java/com/meow/Main.java
+++ src/main/java/com/meow/Main.java
@@ -3,8 +3,14 @@ package com.meow;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,15 +18,22 @@ public class Main {
     public static void main(String[] args) {
         JavaSparkContext sc = new JavaSparkContext("local[*]", "applicationName");
         SQLContext sqlContext = new SQLContext(sc);
-        JavaRDD<Person> personRdd = sc.textFile("src/main/resources/data/persons.txt")
+
+        List<StructField> fields = new ArrayList<>();
+        fields.add(DataTypes.createStructField("name", DataTypes.StringType, true));
+        fields.add(DataTypes.createStructField("age", DataTypes.StringType, true));
+        StructType schema = DataTypes.createStructType(fields);
+
+        JavaRDD<Row> personRdd = sc.textFile("src/main/resources/data/persons.txt")
                 .map(line -> {
                     String[] colums = line.split(",");
-                    return new Person(colums[0], Integer.parseInt(colums[1]));
+                    return RowFactory.create(colums[0], colums[1]);
                 });
-        DataFrame df = sqlContext.createDataFrame(personRdd, Person.class);
+
+        DataFrame df = sqlContext.createDataFrame(personRdd, schema);
         df.registerTempTable("people");
 
-        DataFrame teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19");
+        DataFrame teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 21");
         List<String> teens = teenagers.javaRDD()
                 .map(row -> "Name : " + row.getString(0))
                 .collect();
