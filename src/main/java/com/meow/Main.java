4e80319ef975636f3420e26b25bed55902bb3d78
Antonin NYCZ <antonin.nycz-prestataire@bforbank.com>
2015-12-17 15:57:24 +0100

    Exemple 1 JSON SQL

--- src/main/java/com/meow/Main.java
+++ src/main/java/com/meow/Main.java
@@ -1,31 +1,17 @@
 package com.meow;
 
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
 
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         JavaSparkContext sc = new JavaSparkContext("local[*]", "applicationName");
-        JavaRDD<String> blRDD = sc.textFile("src/main/resources/data/boite aux lettres.csv")
-                // Suppression de la ligne de header
-                .filter(line -> !line.startsWith("CO_MUP"))
-                .cache();
-        blRDD.count();
-
-        Map<String, Integer> blByCityMap = blRDD
-                .map(line -> line.split(";"))
-                .mapToPair(fields -> new Tuple2<>(fields[4], 1))
-                .reduceByKey((x, y) -> x + y)
-                .sortByKey()
-                .collectAsMap();
-
-        blByCityMap
-                .entrySet()
-                .stream()
-                .sorted((o1, o2) -> o1.getKey().compareTo(o2.getKey()))
-                .forEach(entry -> System.out.println(entry.getKey() + " : " + entry.getValue()));
+        SQLContext sqlContext = new SQLContext(sc);
+        DataFrame df = sqlContext.read().json("src/main/resources/data/arbres.json");
+        df.show();
+        df.printSchema();
+        df.select("fields.nom_commun", "fields.famille").show();
     }
 }
