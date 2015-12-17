28e013fcebf59107e03d21719c66bfd313e830c6
Antonin NYCZ <antonin.nycz-prestataire@bforbank.com>
2015-12-17 15:17:39 +0100

    Exemple Spark Core

--- src/main/java/com/meow/Main.java
+++ src/main/java/com/meow/Main.java
@@ -1,13 +1,19 @@
 package com.meow;
 
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 public class Main {
     public static void main(String[] args) {
-        JavaSparkContext sc = new JavaSparkContext("local", "cats");
-        sc.textFile("src/main/resources/data/cats.csv")
-                .filter(line -> !line.startsWith("geom"))
+        JavaSparkContext sc = new JavaSparkContext("local[*]", "applicationName");
+        JavaRDD<String> blRDD = sc.textFile("src/main/resources/data/boite aux lettres.csv")
+                // Suppression de la ligne de header
+                .filter(line -> !line.startsWith("CO_MUP"))
+                .cache();
+        blRDD.count();
+
+        blRDD
                 .map(line -> line.split(";"))
                 .mapToPair(fields -> new Tuple2<>(fields[4], 1))
                 .reduceByKey((x, y) -> x + y)
